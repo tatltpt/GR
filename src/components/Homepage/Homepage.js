4190a2493679e50352fea727c05aa01982a74d61
NGUYENVANHIEU98 <amazinghieu98@gmail.com>
2021-04-29 05:47:54 +0700

    Init form

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Homepage.css";
 import {Col, Row, Card, Image} from "react-bootstrap";
 import Header from '../Layout/Header'
-
+import Carousel from 'react-bootstrap/Carousel'
 class Homepage extends Component {
   render() {
       return (
@@ -10,19 +10,52 @@ class Homepage extends Component {
               <Header/>
               <div class="page-content">
               <Card>
-                <Image src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" fluid />                
+              <Carousel>
+                    <Carousel.Item>
+                      <img
+                        className="img d-block w-100"
+                        src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6"                        alt="First slide"
+                      />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="img d-block w-100"
+                        src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6"                        alt="Second slide"
+                      />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="img d-block w-100" 
+                        src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6"                        alt="Third slide"
+                      />
+                    </Carousel.Item>
+                  </Carousel>       
                   <Row>
                     <Col xs={6} md={3}>
-                      <Image src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
                     </Col>
                     <Col xs={6} md={3}>
-                      <Image src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
                     </Col>
                     <Col xs={6} md={3}>
-                      <Image src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
                     </Col>
                     <Col xs={6} md={3}>
-                      <Image src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={6} md={3}>
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                    </Col>
+                    <Col xs={6} md={3}>
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                    </Col>
+                    <Col xs={6} md={3}>
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
+                    </Col>
+                    <Col xs={6} md={3}>
+                      <Image className="img2" src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6" thumbnail />
                     </Col>
                   </Row>
               </Card>
--- src/components/ListEvent/ListEvent.js
+++ src/components/ListEvent/ListEvent.js
@@ -5,22 +5,48 @@ class ListEvent extends Component {
 
     render() {
         return (
-            <>
+            <div>
                 <Header/>
-               <div>
-                   Create User Name
+                <div className="div-border">
+                    <div>
+                        Create User Name
+                        </div>
+                        <img className="img"
+                            src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6"
+                        />
+                    <div>
+                    <span className="span-bold">Giai chay quoc te</span><br/>
+                        <span>Ngay to chuc: 01/01/2021</span><br/>
+                        <span>Dia diem: Ha Noi</span>
+                    </div>
                 </div>
-               <div>
-                    <img className="img"
-                        src="https://bibpix.net/photo?race_id=vhm2020&bib_type=all&img=#cl-group-2"
-                    />
-               </div>
-               <div>
-                   <span>Giai chay quoc te</span><br/>
-                   <span>Ngay to chuc</span><br/>
-                   <span>Dia diem</span>
-               </div>
-            </>
+                <div className="div-border">
+                    <div>
+                        Create User Name
+                        </div>
+                        <img className="img"
+                            src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6"
+                        />
+                    <div>
+                    <span className="span-bold">Giai chay quoc te</span><br/>
+                        <span>Ngay to chuc: 02/02/2021</span><br/>
+                        <span>Dia diem: Da Nang</span>
+                    </div>
+                </div>
+                <div className="div-border">
+                    <div>
+                        Create User Name
+                        </div>
+                        <img className="img"
+                            src="https://img.bibpix.net/photos/thumb/5f99913e444e670e2b173cc3/4a73f0104bba26a623464f0b039f7dff.jpg?v=6"
+                        />
+                    <div>
+                        <span className="span-bold">Giai chay quoc te</span><br/>
+                        <span>Ngay to chuc: 03/03/2021</span><br/>
+                        <span>Dia diem: Ho Chi Minh</span>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
